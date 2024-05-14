a22c03c366c6cd8238f62e5d40faa7975f063abb
dhavalJB <74847516+dhavalJB@users.noreply.github.com>
2024-05-14 18:52:03 +0530

    aa

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
