1a8f9197cc613a098c577544b34e02c8fdd23827
dhavalJB <74847516+dhavalJB@users.noreply.github.com>
2024-05-18 10:57:33 +0530

    બ

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
+const path = require('path');
 const usersRouter = require('./routes/users');
 
 dotenv.config();
@@ -13,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const REACT_APP_BACKEND_URL = 'https://pimatribend.onrender.com'; // Add this line
+const REACT_APP_BACKEND_URL = 'https://pimatribend.onrender.com'; 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
@@ -24,12 +25,8 @@ app.use('/api/authenticate', async (req, res, next) => {
     const authResult = req.body;
     console.log('Authentication Result:', authResult);
     
-    // Handle authentication result here
-    
-    // Example: Generate JWT token
     const accessToken = jwt.sign({ user: authResult.user }, process.env.JWT_SECRET);
     
-    // Return token to client
     res.json({ accessToken, user: authResult.user });
   } catch (error) {
     console.error('Authentication failed:', error);
@@ -55,14 +52,12 @@ app.use('/api/pi', async (req, res, next) => {
 app.post('/api/handleIncompletePayment', (req, res) => {
   const payment = req.body;
   console.log('Handling incomplete payment:', payment);
-  // Handle incomplete payment here
   res.sendStatus(200);
 });
 
 app.post('/api/approvePayment', (req, res) => {
   const paymentId = req.body.paymentId;
   console.log('Approving payment:', paymentId);
-  // Approve payment
   platformAPIClient.post(`/payments/${paymentId}/approve`)
     .then(() => res.sendStatus(200))
     .catch(error => {
@@ -74,13 +69,22 @@ app.post('/api/approvePayment', (req, res) => {
 app.post('/api/completePayment', (req, res) => {
   const { paymentId, txid } = req.body;
   console.log('Completing payment:', paymentId, 'with TXID:', txid);
-  // Complete payment
   platformAPIClient.post(`/payments/${paymentId}/complete`, { txid })
     .then(() => res.sendStatus(200))
     .catch(error => {
       console.error('Error completing payment:', error.response.data);
       res.status(error.response.status).send(error.response.data);
     });
+});
+
+// Serving Images
+app.get('/api/image/:filename', (req, res) => {
+  const filePath = path.join(__dirname, 'uploads', req.params.filename);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send('Image not found');
+    }
+  });
 });
 
 app.use('/api/users', usersRouter);
